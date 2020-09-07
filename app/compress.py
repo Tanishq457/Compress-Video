719e0cc5333bf46caf1d91d6d7cebac6a7daa1da
bhall <bhalla.tanishq2000@gmail.com>
2020-09-07 10:39:42 +0530

    first commit

---
+++ app/compress.py
@@ -0,0 +1,141 @@
+import cv2
+import numpy as np
+import sys, importlib, os, glob, shutil
+
+if __name__ == "__main__":
+    try:
+        spam_spec = importlib.util.find_spec("filetype")
+        if spam_spec is None:
+            raise(Exception)
+        
+
+    except:
+        print('This program requires filetype package to run. Please install it through: \npip install filetype')
+        exit()
+        
+import filetype
+
+
+
+def compressFrames(video, ratio):
+    
+    try:
+        os.mkdir('temp')
+    except Exception as e:
+        print('Error creating directory temp' + str(e), file=sys.stderr)
+        return 
+
+    cap = cv2.VideoCapture(video)
+
+    if not cap.isOpened():
+        print(f'Error Opening File {video}', file=sys.stderr)
+        return
+
+    if not cap.isOpened():
+        print(f'Error Opening File {video}', file=sys.stderr)
+        return
+    
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    
+
+    try:
+        i=0
+        while True:
+            print('In while True!', file=sys.stderr)
+          
+            ret, frame = cap.read()
+            print('Frame read!', file=sys.stderr)
+            if(not ret):
+                print('Can\'t capture frames/Video End', file=sys.stderr)
+                break
+            i+=1
+            height, width, layers =  frame.shape
+            print('Shape taken', file=sys.stderr)
+            height=int(height*(ratio/100))
+            width=int(width*(ratio/100))
+            compressedframe = cv2.resize(frame, (width, height))
+            print('Resizing!', file=sys.stderr)
+            print(f'{width} {height} {fps}', file=sys.stderr)
+            
+            cv2.imwrite('./temp/temp_frame_'+ str(i) + '.jpg', compressedframe)
+
+        return width, height, fps
+    
+    except Exception as e:
+        print('In except' + str(e), file=sys.stderr)
+        cap.release()
+        cv2.destroyAllWindows()
+        return
+
+        cap.release()
+
+def combineFramesAndSaveVideo(video, width, height, fps):
+
+    images = [image for image in os.listdir('./temp')]
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    
+    name = ''
+    if(os.path.splitext(video)[1] == '' ):
+        name = 'compressed_' + video + '.mp4'
+    else:
+        name = 'compressed_' + video
+    compressedVideo = cv2.VideoWriter(name, fourcc, fps, (width, height))
+    images = glob.glob('./temp/*.jpg')
+
+    images = [os.path.splitext(os.path.basename(images[i]))[0] for i in range(len(images))]
+    images = sorted(images, key=lambda x: int(x[11:]))
+
+    for filename in images:
+        img = cv2.imread('./temp/' + filename + '.jpg')
+        compressedVideo.write(img)
+
+    compressedVideo.release()
+
+    try:
+        shutil.rmtree('./temp')
+    
+    except:
+        print('Error removing temp files', file=sys.stderr)
+
+    return name
+
+
+if __name__ == "__main__":
+    
+    try:
+        spam_spec = importlib.util.find_spec("filetype")
+        if spam_spec is None:
+            raise(Exception)
+        
+    except:
+        print('This program requires filetype package to run. Please install it through: \npip install filetype')
+        exit()
+
+    import filetype
+
+    if(len(sys.argv) < 3):
+        print(f'Insufficient Arguments - {len(sys.argv)} given, 3 required.\nExample: python convert.py filename')
+        exit()
+
+    if(sys.argv[1] not in os.listdir()):
+        print('Error: File does not exist')
+        exit()
+
+
+    if(type(int(sys.argv[2])) != type(1) or int(sys.argv[2]) not in range(1,100)):
+        print('Error: Invalid ratio given')
+        exit()
+    video = sys.argv[1]
+    ratio = int(sys.argv[2])
+
+    if filetype.guess(video).mime.split('/')[0] != 'video':
+        print('Given file is not a video file. Please give a video file.')
+        exit()
+    if('temp' in os.listdir()):
+        print('Please rename the already present temp folder and then run again')
+        exit()
+
+    width, height, fps = compressFrames(video, ratio)
+
+    combineFramesAndSaveVideo(video, width, height, fps)
---
+++ app/main.py
@@ -0,0 +1,99 @@
+import sys, os, shutil
+from os.path import basename
+
+import smtplib
+
+
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+
+import filetype
+
+from compress import combineFramesAndSaveVideo, compressFrames
+
+
+def send_mail(
+    send_from, send_to, subject, text, files=None, server="smtp.gmail.com:587"
+):
+    msg = MIMEMultipart()
+    msg["Date"] = formatdate(localtime=True)
+    msg["Subject"] = subject
+    msg.attach(MIMEText(text))
+
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(fil.read(), Name=basename(f))
+        # After the file is closed
+        part["Content-Disposition"] = 'attachment; filename="%s"' % basename(f)
+        msg.attach(part)
+
+    username = os.environ.get('USERNAME', None)
+    password = os.environ.get('PASSWORD', None)
+    server = smtplib.SMTP("smtp.gmail.com:587")
+    server.starttls()
+    server.login(username, password)
+    server.send_message(msg=msg, from_addr=send_from, to_addrs=send_to)
+    # server.sendmail(send_from, send_to, msg)
+    server.quit()
+
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "."
+SENDER_EMAIL = "bhalla.tanishq2000@gmail.com"
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+'''Home Page of Application'''
+@app.route("/")
+def upload_file():
+    return render_template("home.html")
+
+
+'''Upload point of Application'''
+@app.route("/upload", methods=["GET", "POST"])
+def upload_files():
+    if request.method == "POST":
+
+        if "file" not in request.files:
+            return "No file part"
+        f = request.files["file"]
+        if f.filename == "":
+            return "No selected file"
+
+        ratio = int(request.form['ratio'])
+        
+        if(type(int(ratio)) != type(1) or int(ratio) not in range(1,100)):
+            print('Error: Invalid ratio given', file=sys.stderr)
+            return 'Error: Invalid ratio given'
+
+        fileName = secure_filename(f.filename)
+
+        f.save(fileName)
+        if not filetype.video_match("./" + fileName):
+            print('Error: Not a video file', file=sys.stderr)
+            return "Given file is not a video file. Please give a video file."
+        output = compressFrames(fileName, ratio)
+        print("output: " + str(output), file=sys.stderr)
+        if output is None:
+            shutil.rmtree("./temp")
+            print('Error: Output is None', file=sys.stderr)
+            return "Error"
+        width, height, fps = output
+        fileName2 = combineFramesAndSaveVideo(fileName, width, height, fps)
+
+        send_mail(SENDER_EMAIL, SENDER_EMAIL, "Hi", "Hi There", [fileName2])
+        os.remove(fileName)
+        os.remove(fileName2)
+        return "File Emailed Successfully"
+    return "No File/Email given"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=4000)
